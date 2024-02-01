eb6c59cfefa85b4c7d356c61fd9f90970a764554
scgines <giness@uci.edu>
2024-01-31 17:44:50 -0800

    Comments for ItemBehavior Script

--- Assets/Scripts/Player/ItemBehavior.cs
+++ Assets/Scripts/Player/ItemBehavior.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class ItemBehavior : MonoBehaviour ///Team members that contributed to this script: Samuel Gines
 {
-    [SerializeField] private float acceleration;
-    [SerializeField] private float speed;
-    [SerializeField] private Transform target;
-    private void OnTriggerStay(Collider col)
+    [SerializeField] private float acceleration; // The increase in speed per frame
+    [SerializeField] private float speed; // how fast the object is going
+    [SerializeField] private Transform target; // the target that will be pulled towards the player
+    private void OnTriggerStay(Collider col) // When a collider enters the gameobjects trigger collider
     {
-        if (col.gameObject.tag == "Item") 
+        if (col.gameObject.tag == "Item") // If the object is an item...
         {
-            speed += acceleration * Time.deltaTime;
-            target.position = Vector3.MoveTowards(target.position, transform.position, speed);
+            speed += acceleration * Time.deltaTime; // Accelerate the target per frame
+            target.position = Vector3.MoveTowards(target.position, transform.position, speed); // move the target towards the player
         }
     }
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other) // When the item is no longer in range...
     {
-        speed = 0;
+        speed = 0; // Stop the object
     }
 }
